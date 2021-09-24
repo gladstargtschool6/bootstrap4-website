35253bbbd08d3b33946fa47d07d4b812ee117530
Joseph Moshood <gladstargtschool6@gmail.com>
2021-09-24 06:42:32 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
  $('.header').height($(window).height());
-
+ $("button").click(function ( {  
+$.get("https://app.schoology.com/school/2654352006/", function (data, document, quotes) { 
  $(".navbar a").click(function(){
  	$("body,html").animate({
  		scrollTop:$("#" + $(this).data('value')).offset().top
